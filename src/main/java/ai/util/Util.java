f61a9054af40f6922780288cee55f4b72a27d793
makiasake <alexandre.maki@hotmail.com>
2026-03-03 21:32:13 -0300

    - Implemented run command method.     - Added properties and a method to read them.     - Added kokoro test calls.

--- src/main/java/ai/util/Util.java
+++ src/main/java/ai/util/Util.java
@@ -1,15 +1,45 @@
 package ai.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Properties;
+
 public class Util {
 
-	public static void clear() {
+	public String readProperty(final String property) throws IOException {
 
-		try {
-			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-		} catch (Exception ex) {
-			ex.printStackTrace();
+		Properties prop = new Properties();
+		try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+			if (input == null)
+				throw new IOException("config.properties not found!");
+
+			prop.load(input);
+			return prop.getProperty(property);
+		} catch (IOException ex) {
+			throw ex;
 		}
 
+	}
+
+	public void runCommand(final List<String> command) throws IOException, InterruptedException {
+
+		ProcessBuilder pb = new ProcessBuilder(command);
+		pb.redirectErrorStream(true);
+		Process process = pb.start();
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		String line;
+		StringBuilder output = new StringBuilder();
+		while ((line = reader.readLine()) != null) {
+			output.append(line).append("\n");
+		}
+
+		int exitCode = process.waitFor(); // Wait for the process to complete
+		System.out.println("Python script exited with code: " + exitCode);
+		System.out.println("Script Output:\n" + output.toString());
 	}
 
 }
---
+++ src/test/java/ai/Test.java
@@ -0,0 +1,43 @@
+package ai;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+
+import ai.util.Util;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		try {
+			callKokoroToGenerateAudio();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	private void callTestScript() throws IOException, InterruptedException {
+		new Util().runCommand(Arrays.asList(StringUtils.split(new Util().readProperty("test.script.path"), " ")));
+	}
+
+	private static void callKokoroScript() throws IOException, InterruptedException {
+		final String command = "py -m uv run --directory=" + new Util().readProperty("kokoro.path")
+				+ " kokoro-tts --help-voices";
+		new Util().runCommand(Arrays.asList(StringUtils.split(command, " ")));
+	}
+
+	private static void callKokoroToGenerateAudio() throws IOException, InterruptedException {
+		final String command = "py -m uv run -m --directory=" + new Util().readProperty("kokoro.path") + " kokoro_tts "
+				+ new Util().readProperty("text.root.folder.path") + "/test-tts.txt "
+				+ new Util().readProperty("wav.root.folder.path") + "/" + System.currentTimeMillis() + ".wav"
+				+ " --speed 1.0 --lang en-us --voice af_sarah --format wav";
+
+		System.out.println("Running: " + command);
+
+		new Util().runCommand(Arrays.asList(StringUtils.split(command, " ")));
+	}
+
+}
